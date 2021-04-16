07f63aaecbf6dd12923d1f35b66d5c927b8cf970
Jinal Era <66786285+jinalEra@users.noreply.github.com>
2021-04-16 16:27:38 +0530

    Update urls.py

--- todos/urls.py
+++ todos/urls.py
@@ -1,1 +1,10 @@
 
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+app_name = 'todos'
+urlpatterns = [
+    path('', views.index, name= "index" ),
+]
+
